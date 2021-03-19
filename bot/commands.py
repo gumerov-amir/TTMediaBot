e0e66052724dc93d657212590260bc32226fcf86
Gumerov Amir Eduardovich <=>
2021-03-19 10:51:55 +0300

    TT wraper changes, sending messages

--- bot/commands.py
+++ bot/commands.py
@@ -7,19 +7,27 @@ from .streamer import Streamer
 from .player import Mode, State
 
 class ProcessCommand(object):
-    def __init__(self, player, ttclient, services, default_service):
+    def __init__(self, player, ttclient, services, default_service, admins, banned_users):
         self.player = player
         self.ttclient = ttclient
         self.services = services
         self.service = default_service
         self.streamer = Streamer(self.services)
+        self.admins = admins
+        self.banned_users = banned_users
         self.commands_dict = {'p': self.play_pause, 's': self.stop, 'm': self.mode,     'sb': self.seek_back, 'sf': self.seek_forward, 'r': self.rate, 'v': self.volume, 'u': self.play_by_url, 'h': self.help, 'n': self.next, 'b': self.back, 'c': self.change_service}
         self.admin_commands_dict = {'girl': lambda arg: 'Настенька', "cn": self.change_nickname}
 
 
-    def __call__(self, message, is_admin):
-        self.is_admin = is_admin
-        try:
+    def __call__(self, message, user):
+        if user.szUsername in self.banned_users:
+            return _('You are banned')
+        self.user = user
+        self.is_admin = user.szUsername in self.admins
+        if not self.is_admin:
+            if user.nChannelID != self.ttclient.getMyChannelID():
+                return _('You aren\'t in channel with bot')
+        try:    
             command = re.findall('[a-z]+', message.split(' ')[0].lower())[0]
         except IndexError:
             return self.help()
@@ -27,7 +35,7 @@ class ProcessCommand(object):
         try:
             if command in self.commands_dict:
                 return self.commands_dict[command](arg)
-            elif is_admin and command in self.admin_commands_dict:
+            elif self.is_admin and command in self.admin_commands_dict:
                 return self.admin_commands_dict[command](arg)
             else:
                 return _('Unknown command.\n') + self.help()
@@ -37,11 +45,12 @@ class ProcessCommand(object):
 
     def play_pause(self, arg):
         if arg:
-            s = time.time()
+            self.ttclient.send_message(_('it is finding'), self.user.nUserID)
             track_list = self.service.search(arg)
-            print(time.time() - s)
             if track_list:
                 self.player.play(track_list)
+                self.ttclient.send_message(_("{} offered {}").format(self.user.szNickname, track_list[0].name), type=2)
+                return _('Playing {}').format(track_list[0].name)
             else:
                 return _('not found')
         else:
@@ -52,9 +61,9 @@ class ProcessCommand(object):
 
 
     def rate(self, arg):
-        '''Изменяет скорость'''
         if not arg:
             self.player._vlc_player.set_rate(1)
+            return
         try:
             rate_number = abs(float(arg))
             if rate_number > 0 and rate_number <= 4:
@@ -80,11 +89,9 @@ class ProcessCommand(object):
             return self.help()
 
     def stop(self, arg):
-        '''Останавливает аудио'''
         self.player.stop()
 
     def volume(self, arg):
-        '''Изменяет Громкость'''
         try:
             volume = int(arg)
             if volume >= 0 and volume <= 100:
