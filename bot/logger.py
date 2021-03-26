f77a1281c1e49ffe56e247e2e62cce3d6427841b
Gumerov Amir Eduardovich <=>
2021-03-26 12:08:57 +0300

    logger  fic

--- bot/logger.py
+++ bot/logger.py
@@ -4,17 +4,17 @@ import sys
 
 
 def initialize_logger(config):
-    root_logger = logging.getLogger('root')
     level = logging.getLevelName(config['level'])
-    root_logger.setLevel(level)
     formatter = logging.Formatter(config['format'])
+    handlers = []
     if config['mode'] >= 2:
         rotating_file_handler = RotatingFileHandler(filename=config['file_name'], mode='a', maxBytes=config['max_file_size'] * 1024, backupCount=config['backup_count'])
         rotating_file_handler.setFormatter(formatter)
         rotating_file_handler.setLevel(level)
-        root_logger.addHandler(rotating_file_handler)
+        handlers.append(rotating_file_handler)
     if config['mode'] == 1 or config['mode'] == 3:
         stream_handler = logging.StreamHandler(sys.stdout)
         stream_handler.setFormatter(formatter)
         stream_handler.setLevel(level)
-        root_logger.addHandler(stream_handler)
+        handlers.append(stream_handler)
+    logging.basicConfig(level=level, format=config['format'], handlers=handlers)
