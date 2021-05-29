69618a07f7785d159d2a02f21bb94444844b4ac9
Gumerov Amir Eduardovich <=>
2021-05-29 12:24:44 +0300

    localisation fixes

--- bot/connectors/tt_player_connector.py
+++ bot/connectors/tt_player_connector.py
@@ -22,16 +22,19 @@ class TTPlayerConnector(Thread):
                     self.ttclient.enable_voice_transmission()
                     last_track_meta = self.player.track.get_meta()
                     if self.player.track.name:
-                        self.ttclient.change_status_text('{state}: {name}'.format(state=self.player.state.value, name=self.player.track.name))
+                        self.ttclient.change_status_text(_('Playing: {track_name}').format(track_name=self.player.track.name))
                     else:
-                        self.ttclient.change_status_text('{state}: {url}'.format(state=self.player.state.value, url=self.player.track.url))
+                        self.ttclient.change_status_text(_('Playing: {stream_url}').format(stream_url=self.player.track.url))
                 elif self.player.state == State.Stopped:
                     self.ttclient.disable_voice_transmission()
                     self.ttclient.change_status_text('')
                 elif self.player.state == State.Paused:
                     self.ttclient.disable_voice_transmission()
-                    self.ttclient.change_status_text('{state}: {name}'.format(state=self.player.state.value, name=self.player.track.name))
+                    if self.player.track.name:
+                        self.ttclient.change_status_text(_('Paused: {track_name}').format(track_name=self.player.track.name))
+                    else:
+                        self.ttclient.change_status_text(_('Paused: {stream_url}').format(stream_url=self.player.track.url))
             if self.player.track.get_meta() != last_track_meta and last_player_state != State.Stopped:
                 last_track_meta = self.player.track.get_meta()
-                self.ttclient.change_status_text('{state}: {name}'.format(state=self.player.state.value, name=self.player.track.name))
+                self.ttclient.change_status_text('{state}: {name}'.format(state=self.ttclient.status.split(':')[0], name=self.player.track.name))
             time.sleep(vars.loop_timeout)
