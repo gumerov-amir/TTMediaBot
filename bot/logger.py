e50f10f74482814b68e8f5ff6841bbd5a6074bf6
A11CF0 <a11cf0.vk@yandex.ru>
2021-10-24 13:30:25 +0300

    Fix a stupid mistake with log filename variables.

--- bot/logger.py
+++ bot/logger.py
@@ -11,7 +11,7 @@ class Mode(Flag):
     FILE = 2
     STDOUT_AND_FILE = STDOUT|FILE
 
-def initialize_logger(config, file_name):
+def initialize_logger(config, file):
     logging.addLevelName(5, "PLAYER_DEBUG")
     level = logging.getLevelName(config['level'])
     formatter = logging.Formatter(config['format'])
@@ -21,10 +21,8 @@ def initialize_logger(config, file_name):
     except KeyError:
         sys.exit("Invalid log mode name")
     if mode & Mode.FILE == Mode.FILE:
-        if file_name:
-            file_name = file_name
-        else:
-            file_name = config["file_name"]
+        if not file:
+            file = config["file_name"]
         rotating_file_handler = RotatingFileHandler(filename=file, mode='a', maxBytes=config['max_file_size'] * 1024, backupCount=config['backup_count'], encoding='UTF-8')
         rotating_file_handler.setFormatter(formatter)
         rotating_file_handler.setLevel(level)
