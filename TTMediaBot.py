88a0fe2f5b9d799fe9132ebd7f674b250c4a0716
cyrmax <belousov.k.m@ya.com>
2022-03-20 16:44:04 +0300

    Rewrites argument parsing from Click to argparse
    Former-commit-id: 85cbf3f673391eb2ae02f127ad89197ee75a0508

--- TTMediaBot.py
+++ TTMediaBot.py
@@ -2,31 +2,26 @@ from typing import Optional
 
 from os import path
 
-import click
+from argparse import ArgumentParser
 
 from bot import Bot, app_vars
 from bot.config import save_default_file
 from bot.sound_devices import SoundDeviceManager
 
+parser = ArgumentParser()
+parser.add_argument("-c", "--config", help="Path to the configuration file", default=path.join(app_vars.directory, "config.json"))
+parser.add_argument("-C", "--cache", help="Path to the cache file", default=None)
+parser.add_argument("-l", "--log", help="Path to the log file", default=None)
+parser.add_argument("--devices", help="Show available devices and exit", action="store_true")
+parser.add_argument("--default-config", help='Save default config to "config_default.json" and exit', action="store_true")
+args = parser.parse_args()
 
-@click.command()
-@click.option(
-    "-c", "--config", help="Path to the configuration file", default=path.join(app_vars.directory, "config.json")
-)
-@click.option("--cache", help="Path to the cache file", default=None)
-@click.option("--log", help="Path to the log file", default=None)
-@click.option("--devices", help="Show available devices and exit", is_flag=True)
-@click.option(
-    "--default-config",
-    help='Save default config to "config_default.json" and exit',
-    is_flag=True,
-)
 def main(
-    config: str,
-    cache: Optional[str],
-    log: Optional[str],
-    devices: bool,
-    default_config: bool,
+    config: str = args.config,
+    cache: Optional[str] = args.cache,
+    log: Optional[str] = args.log,
+    devices: bool = args.devices,
+    default_config: bool = args.default_config,
 ) -> None:
     if devices:
         bot = Bot(None, None, None)
