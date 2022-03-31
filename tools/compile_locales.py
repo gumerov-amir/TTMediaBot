ea4bd063a72d1b9ef30c8cc91db5279c6170f424
Gumerov Amir Eduardovich <=>
2022-03-31 15:34:43 +0300

    fixed compile_locales.py
    Former-commit-id: 38f03dd47b0c6b86f94e5704a973c1a3f99e5393

--- tools/compile_locales.py
+++ tools/compile_locales.py
@@ -33,7 +33,7 @@ def update():
 
 def compile():
     code = subprocess.call(
-        f"{babel_prefix} compile -d {locale_path} -D {locale_domain}", shell=True
+        f"{babel_prefix} compile -d {locale_path} -D {locale_domain} --previous", shell=True
     )
     if code:
         sys.exit(code)
